fdc7aec09c1e4044eafc708c3d1ea9a3d68b83f6
八妹 <1036749437@qq.com>
2018-04-26 10:20:01 +0800

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 // DEV_HOST: 本地开发URL
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: JSON.stringify(''),
+  BASE_URL: JSON.stringify('你的ip'),
 
-  DEV_HOST: ''
+  DEV_HOST: '你的域名'
 })
